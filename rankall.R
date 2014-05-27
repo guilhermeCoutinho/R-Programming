950a01f21ed58dd7904a5704f2552ef35cbff63d
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2014-05-27 17:02:04 -0300

    Update rankall.R

--- rankall.R
+++ rankall.R
@@ -1,11 +1,11 @@
 rankall = function (outcome , num = "best") {
      isDebugMode = F
      if (isDebugMode){
-          outcome = "heart attack"
-          num = 20
+          outcome = "pneumonia"
+          num = "worst"
      }
-     data = read.csv("outcome-of-care-measures.csv")
-
+     data = read.csv("outcome-of-care-measures.csv" , stringsAsFactors=F)
+     
      if (outcome == "heart attack"){
           outcome = names(data)[11]
      }else if (outcome == "heart failure"){
@@ -17,18 +17,22 @@ rankall = function (outcome , num = "best") {
      }
      data = data[,c("Hospital.Name",outcome,"State")]
      data = data[,][data[,outcome] != "Not Available" ,]
+     data$State = as.factor(data$State)
      output = data.frame()
      if (num == "best"){
           num = 1
      }
-     
+     isWorst = F
+     if (num == "worst"){
+          isWorst = T
+     }
      
      for (i in seq(levels(data$State))){
           i = levels(data$State)[i]
           aux = data[data$State == i ,]
+          aux[,outcome] = as.numeric(aux[,outcome])
           aux = aux[order(aux[outcome] , aux$Hospital.Name) , ]
-     
-          if (num=="worst"){
+          if (isWorst){
                num = nrow(aux)
           }
           output = rbind(output , cbind(toString(aux[num,1]) , toString(aux[1,3]) ))
